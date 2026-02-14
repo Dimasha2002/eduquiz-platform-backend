b0c34a8b3d782c2b600d76b35e409dee0443b1f9
Dimasha2002 <it23832244@my.sliit.lk>
2026-02-14 15:25:20 +0530

    Update

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.NODE_ENV === 'production' 
+    ? process.env.CLIENT_URL 
+    : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
   credentials: true
 }));
 app.use(express.json());
